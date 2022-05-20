a201b88e625fa545bae28fb5f386e6abab2e3233
nohaderf <freda.hon@gmail.com>
2022-05-19 23:36:26 -0400

    added readme

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,12 @@ function Footer(){
             </a></p>
             <p><a href="https://medium.com/@freda.hon" target="_blank">
             <i className="fab fa-medium"></i>
+            </a></p> 
+            <p><a href="mailto:freda.hon@gmail.com" target="_blank">
+            <span>Freda Hon Production <i className="far fa-copyright"></i> </span>
             </a></p>
+
+            
             <p className="company-footer"><img className="logo-footer" src={logo}></img>Life League</p>
         </div>
     )
